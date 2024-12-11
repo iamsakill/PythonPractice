c780f48f203f8115529c37c1220dbd6e485cf258
sakilbabu <69412771+sakilbabu@users.noreply.github.com>
2024-12-11 11:53:35 +0600

    Initial commit

---
+++ if_else.py
@@ -0,0 +1,7 @@
+print("welcome to the rollercoaster")
+height = int(input("what is your height in cm?"))
+
+if height >= 120:
+     print("you can write the rollercoaster")
+else:
+     print("Sorry you have to grow more first")
