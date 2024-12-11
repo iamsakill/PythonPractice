db96eb4bdb34370bd69b8cf6e14860d7dd68b6d4
sakilbabu <69412771+sakilbabu@users.noreply.github.com>
2024-12-11 11:57:44 +0600

    modulo

--- if_else.py
+++ if_else.py
@@ -3,5 +3,12 @@ height = int(input("what is your height in cm?"))
 
 if height >= 120:
      print("you can write the rollercoaster")
+     age = int(input("what is your age?"))
+     if age <= 12:
+          print("pay $5")
+     elif age <= 18:
+          print("pay 7")
+     else:
+          print("5")
 else:
      print("Sorry you have to grow more first")
